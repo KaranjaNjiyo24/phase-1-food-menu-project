158a19367480b7394e805a2308d5006c1ebb9ca0
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-10-18 06:11:38 +0300

    Added function to fetch and display menu items

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
     const foodMenu = document.getElementById('food-menu')
     const baseUrl = 'https://foodish-api.com/api'
+    let menuItems = [];
+    
     //Use our Fetch 
+    for (let i=0; i<10; i++) {
     fetch(baseUrl)
-    .then(res => res.json())
-    .then(data => {
-        const imgFood = document.createElement('img')
-        imgFood.src = data.image
-        imgFood.alt = "Food Image"
-        imgFood.style.width = "200px"
-        imgFood.style.height = "200px";
-        foodMenu.appendChild(imgFood)
-    })
+        .then(res => res.json())
+        .then(data => {
+            const imageUrl =data.image
+            //function to extract category from url
+            const category = extractCategoryFromUrl(imageUrl)
+            //function to get the dishname from url
+            const dishName = formatDishName(category)
+
+            menuItems.push({
+                imageUrl: imageUrl,
+                dishName: dishName,
+                category: category,
+                likes:0,
+                dsilikes:0,
+                liked:false,
+                dislike: false,
+                comments:[]
+            })
+
+            if (menuItems.length === 10) {
+                displayFoodItems()
+            }
+
+        })
+    }
+    function displayFoodItems() {
+        foodMenu.innerHTML = ""
+        menuItems.forEach((item, index) => {
+            const card = document.createElement("div")
+            card.classList.add("food-card")
+
+            card .innerHTML = `
+                <img src="{item.imageUrl}" class="food-image" alt="${item.dishName}">
+                <div class="food-info">
+                    <h3>${item.dishName}</h3>
+                    <p>Category: ${item.category}</p>
+                </div>
+            `
+            foodMenu.appendChild(card)
+        })
+    }
+
+    //Creating the extract category from URL
+    //Using split
+    function extractCategoryFromUrl(imageUrl) {
+        const urlParts = imageUrl.split("/")
+        return urlParts[urlParts.length - 2]
+    }
+
+    //Formatting the dishname to capitlize the first letter
+    // using toUpperCase
+    function formatDishName(category) {
+        return category.charAt(0).toUpperCase() + category.slice(1)
+    }
 })
